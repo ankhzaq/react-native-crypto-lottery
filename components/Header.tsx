9dd75f9ca7887af949477b3b73e7d7d1d068d1d5
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-24 14:00:00 +0200

    feat: header customized and used on index file

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div className="flex items-center space-x-2">
+        <img
+          alt=""
+          className="rounded-full h-20 w-20"
+          src="https://i.imgur.com/4h7mAu7.png"
+        />
+        <div>
+          <h1 className="text-lg text-white font-bold">ZAQUIEL DRAW</h1>
+          <p className="text-xs text-emerald-500 truncate">User...</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="bg-[#091B18] min-h-screen flex flex-col">
       <Head>
         <title>Zaquiel Draw</title>
       </Head>
-
+      <Header />
       <h1>Lets build an awesome Lottery WEB3 Dapp!</h1>
     </div>
   )
