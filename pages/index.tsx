37667a0f86a80806589e6bef0f12b354482c8961
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-09-29 14:30:00 +0200

    loading added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,28 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Login from '../components/Login';
-import { useAddress } from '@thirdweb-dev/react';
+import { useAddress, useContract } from '@thirdweb-dev/react';
 
 const Home: NextPage = () => {
   const address = useAddress();
+  const { contract, isLoading } = useContract(
+    process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+  );
+
+  if (isLoading) {
+    return (
+      <div className="bg-[#091B18] h-screen flex flex-col items-center justify-center">
+        <div className="flex items-center space-x-2 mb-10">
+          <img
+            className="rounded-full h-20 w-20"
+            alt=""
+            src="https://i.imgur.com/4h7mAu7.png"
+          />
+          <h1 className="text-lg text-white font-bold">Loading the ZAQUIEl DRAW</h1>
+        </div>
+      </div>
+    );
+  }
 
   if (!address) return (<Login />);
 
